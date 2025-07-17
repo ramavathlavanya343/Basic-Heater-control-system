0c1236fd9d1be4d35989a3bc33c39fea03a471f5
Lavanya <125568409+ramavathlavanya343@users.noreply.github.com>
2025-07-17 19:12:11 +0530

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -4,122 +4,17 @@
 const int DHT_PIN = 15;
 const int fanPin = 2;
 const int heaterPin = 4;
-const int buzzerPin = 5; 
+const int buzzerPin = 5;
 
 const float TARGET_TEMP = 25.0;
 const float OVERHEAT_TEMP = 35.0;
 const float HYSTERESIS = 0.5;
 
-enum State {
-  IDLE,
-  HEATING,
-  STABILIZING,
-  TARGET_REACHED,
-  OVERHEAT
-};
-
+enum State { IDLE, HEATING, STABILIZING, TARGET_REACHED, OVERHEAT };
 State currentState = IDLE;
 
-DHTesp dhtSensor;
-LiquidCrystal_I2C lcd(0x27, 16, 4);
-
-void updateState(float temp) {
-  switch (currentState) {
-    case IDLE:
-      if (temp >= OVERHEAT_TEMP) currentState = OVERHEAT;
-      else if (temp < TARGET_TEMP - 2) currentState = HEATING;
-      break;
-
-    case HEATING:
-      if (temp >= TARGET_TEMP - HYSTERESIS && temp < OVERHEAT_TEMP)
-        currentState = STABILIZING;
-      else if (temp >= OVERHEAT_TEMP)
-        currentState = OVERHEAT;
-      break;
-
-    case STABILIZING:
-      if (temp >= TARGET_TEMP && temp < OVERHEAT_TEMP)
-        currentState = TARGET_REACHED;
-      else if (temp >= OVERHEAT_TEMP)
-        currentState = OVERHEAT;
-      break;
-
-    case TARGET_REACHED:
-      if (temp >= OVERHEAT_TEMP)
-        currentState = OVERHEAT;
-      else if (temp < TARGET_TEMP - 2)
-        currentState = HEATING;
-      break;
-
-    case OVERHEAT:
-      if (temp < TARGET_TEMP - 3)
-        currentState = IDLE;
-      break;
-  }
-}
-
-void controlSystem(State state) {
-  switch (state) {
-    case IDLE:
-      digitalWrite(heaterPin, LOW);
-      digitalWrite(fanPin, LOW);
-      digitalWrite(buzzerPin, LOW);
-      lcd.setCursor(0, 0); lcd.print("State: IDLE       ");
-      break;
-
-    case HEATING:
-      digitalWrite(heaterPin, HIGH);
-      digitalWrite(fanPin, LOW);
-      digitalWrite(buzzerPin, LOW);
-      lcd.setCursor(0, 0); lcd.print("State: HEATING    ");
-      break;
-
-    case STABILIZING:
-      digitalWrite(heaterPin, LOW);
-      digitalWrite(fanPin, LOW);
-      digitalWrite(buzzerPin, LOW);
-      lcd.setCursor(0, 0); lcd.print("State: STABILIZING");
-      break;
-
-    case TARGET_REACHED:
-      digitalWrite(heaterPin, LOW);
-      digitalWrite(fanPin, LOW);
-      digitalWrite(buzzerPin, LOW);
-      lcd.setCursor(0, 0); lcd.print("State: TARGET OK  ");
-      break;
-
-    case OVERHEAT:
-      digitalWrite(heaterPin, LOW);
-      digitalWrite(fanPin, HIGH);
-      digitalWrite(buzzerPin, HIGH);
-      lcd.setCursor(0, 0); lcd.print("State: OVERHEAT!! ");
-      break;
-  }
-}
-
-void displayData(float temp, float hum) {
-  lcd.setCursor(0, 1); lcd.print("Temp: "); lcd.print(temp); lcd.print(" C   ");
-  lcd.setCursor(0, 2); lcd.print("Hum : "); lcd.print(hum); lcd.print(" %   ");
-  lcd.setCursor(0, 3);
-  lcd.print("Heater: "); lcd.print(digitalRead(heaterPin) ? "ON " : "OFF");
-  lcd.setCursor(12, 3);
-  lcd.print("Fan: "); lcd.print(digitalRead(fanPin) ? "ON " : "OFF");
-
-  Serial.print("Temp: "); Serial.print(temp, 1);
-  Serial.print(" °C | Hum: "); Serial.print(hum, 1);
-  Serial.print(" % | State: "); Serial.println(getStateName(currentState));
-}
-
-String getStateName(State state) {
-  switch (state) {
-    case IDLE: return "IDLE";
-    case HEATING: return "HEATING";
-    case STABILIZING: return "STABILIZING";
-    case TARGET_REACHED: return "TARGET_REACHED";
-    case OVERHEAT: return "OVERHEAT";
-    default: return "UNKNOWN";
-  }
-}
+DHTesp dht;
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // Changed to 2 lines for simplicity
 
 void setup() {
   pinMode(fanPin, OUTPUT);
@@ -129,19 +24,39 @@ void setup() {
   lcd.init();
   lcd.backlight();
 
-  dhtSensor.setup(DHT_PIN, DHTesp::DHT22);
+  dht.setup(DHT_PIN, DHTesp::DHT22);
   Serial.begin(115200);
-  delay(1000);
 }
 
 void loop() {
-  TempAndHumidity data = dhtSensor.getTempAndHumidity();
+  TempAndHumidity data = dht.getTempAndHumidity();
   float temp = data.temperature;
   float hum = data.humidity;
 
-  updateState(temp);
-  controlSystem(currentState);
-  displayData(temp, hum);
+  // State update logic
+  if (currentState == IDLE && temp < TARGET_TEMP - 2) currentState = HEATING;
+  else if (currentState == HEATING && temp >= TARGET_TEMP - HYSTERESIS) currentState = STABILIZING;
+  else if (currentState == STABILIZING && temp >= TARGET_TEMP) currentState = TARGET_REACHED;
+  else if (temp >= OVERHEAT_TEMP) currentState = OVERHEAT;
+  else if (currentState == OVERHEAT && temp < TARGET_TEMP - 3) currentState = IDLE;
+  else if (currentState == TARGET_REACHED && temp < TARGET_TEMP - 2) currentState = HEATING;
+
+  // Output control
+  digitalWrite(heaterPin, (currentState == HEATING) ? HIGH : LOW);
+  digitalWrite(fanPin, (currentState == OVERHEAT) ? HIGH : LOW);
+  digitalWrite(buzzerPin, (currentState == OVERHEAT) ? HIGH : LOW);
+
+  // LCD display
+  lcd.setCursor(0, 0);
+  lcd.print("T:"); lcd.print(temp); lcd.print("C ");
+  lcd.print("H:"); lcd.print(hum); lcd.print("% ");
+
+  lcd.setCursor(0, 1);
+  if (currentState == IDLE) lcd.print("IDLE         ");
+  else if (currentState == HEATING) lcd.print("HEATING      ");
+  else if (currentState == STABILIZING) lcd.print("STABILIZING  ");
+  else if (currentState == TARGET_REACHED) lcd.print("TARGET OK    ");
+  else if (currentState == OVERHEAT) lcd.print("OVERHEAT!!!  ");
 
   delay(1000);
 }
